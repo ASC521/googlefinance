b8ca389ff262e5fda64e5ca03906001c68ec2a59
ASC521 <Blast#6usurp#cash5>
2017-06-17 12:28:58 -0400

    version update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='googlefinance',
-      version='0.2',
+      version='0.3',
       description='Package to retrive current quotes, historical prices, and descriptive details from Google Finance.',
       author='Alex Curto',
       license='MIT',
