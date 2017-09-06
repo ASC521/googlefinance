b3fbc1889263fd5bcae1542cb290584880703a53
acurto <curto.alexander@gmail.com>
2017-09-06 10:07:16 -0400

    version update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='googlefinance',
-      version='0.5',
+      version='0.6',
       description='Package to retrive current quotes, historical prices, and descriptive details from Google Finance.',
       author='Alex Curto',
       license='MIT',
